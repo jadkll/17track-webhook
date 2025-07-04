08f4c83f694195f55b940fe3f23545205487657d
jadkll <jadookhalil6@gmail.com>
2025-07-04 10:32:56 +0700

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-
 from fastapi import FastAPI, Request
-import json
+import uvicorn
+import os
 
 app = FastAPI()
 
 @app.post("/")
-async def receive_webhook(request: Request):
-    data = await request.json()
-    print("📦 Données reçues :", json.dumps(data, indent=2))
-    return {"message": "Webhook reçu"}
+async def webhook(request: Request):
+    payload = await request.json()
+    print("📦 Données reçues :", payload)
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
