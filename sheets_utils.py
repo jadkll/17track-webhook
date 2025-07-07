162ca743fa3bb5fffe6981ec10a73b9e78a2e027
jadkll <jadookhalil6@gmail.com>
2025-07-07 13:25:14 +0700

    Update sheets_utils.py

--- sheets_utils.py
+++ sheets_utils.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # --- CONFIGURATION ---
 SCOPE = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-CREDS_FILE = "C:\Users\jadoo\Downloads\nice-compass-465205-p9-59854cd3bf7e.json"  # ← remplace par le chemin vers le fichier .json
+CREDS_FILE = os.path.join(os.path.dirname(__file__), "credentials.json")  # ← remplace par le chemin vers le fichier .json
 SPREADSHEET_ID = "1H9b6LMqhQi07_FzGzbeRz3qg5TE5AxlEeXqwy8nOjnw"  # ← remplace par l'ID (dans l'URL de ta Google Sheet)
 
 # --- INITIALISATION ---
@@ -12,7 +12,7 @@ gc = gspread.authorize(credentials)
 sheet = gc.open_by_key(SPREADSHEET_ID).sheet1  # On suppose que tu veux la première feuille
 
 # --- FONCTIONS UTILISABLES ---
-def enregistrer_suivi(numero, user_id):
+def ajouter_suivi(numero, user_id):
     lignes = sheet.get_all_records()
     for ligne in lignes:
         if ligne["numero"] == numero:
@@ -20,7 +20,7 @@ def enregistrer_suivi(numero, user_id):
 
     sheet.append_row([numero, str(user_id), "", "", "", ""])  # Ajoute une ligne vide prête à être remplie
 
-def maj_suivi(numero, location, time, description, status):
+def mettre_a_jour_suivi(numero, location, time, description, status):
     lignes = sheet.get_all_records()
     for i, ligne in enumerate(lignes):
         if ligne["numero"] == numero:
@@ -28,7 +28,7 @@ def maj_suivi(numero, location, time, description, status):
             sheet.update(f"C{row}:G{row}", [[location, time, description, status, "OK"]])
             break
 
-def trouver_user_par_numero(numero):
+def get_user_id(numero):
     lignes = sheet.get_all_records()
     for ligne in lignes:
         if ligne["numero"] == numero:
